27dc714b24343c3a2dff567973e692b3f99e6853
EWIllC <74640264+EWIllC@users.noreply.github.com>
2022-03-04 12:04:49 -0500

    testing heroku cli with postgre

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,25 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
+const Sequelize = require('sequelize');
+const pkg = require('../../package.json');
+const { Client } = require("pg");
+
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
@@ -11,14 +31,14 @@ if(process.env.LOGGING === 'true'){
   delete config.logging
 }
 
-//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if(process.env.DATABASE_URL){
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  };
-}
+// //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+// if(process.env.DATABASE_URL){
+//   config.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   };
+// }
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
